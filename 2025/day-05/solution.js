e49fe260cf85000df7a8be0ff32455625a1ea898
John Sigg <jdsigg2@gmail.com>
2025-12-04 18:59:40 -0500

    Adjust wording for 2025, Day 3

---
+++ 2025/day-05/solution.js
@@ -0,0 +1,13 @@
+const fs = require('node:fs');
+const input = fs.readFileSync('./input.txt', 'utf-8');
+
+function part1(input) {
+
+}
+
+function part2(input) {
+
+}
+
+console.log("Part 1:", part1(input));
+console.log("Part 2:", part2(input));
