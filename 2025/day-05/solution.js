7c8199aefbf9c6e0826462f22de822007ed319ff
John Sigg <jdsigg2@gmail.com>
2025-12-05 10:25:37 -0500

    2025, Day 5

--- 2025/day-05/solution.js
+++ 2025/day-05/solution.js
@@ -1,13 +1,53 @@
+const Range = require('../../common/structures/range').Range;
 const fs = require('node:fs');
 const input = fs.readFileSync('./input.txt', 'utf-8');
 
-function part1(input) {
-
+function parseInput(input) {
+    let [ranges, ingredients] = input.split('\n\n');
+    return [
+        ranges.split('\n')
+            .map(line => line.split('-'))
+            .map(([l, r]) => new Range(l, r, /* isBigInt= */ true)),
+        ingredients.split('\n')
+            .map(line => BigInt(line))
+    ]
 }
 
-function part2(input) {
-
+function part1(ranges, ingredients) {
+    // Fine to brute force - 192 ranges, 1,000 ingredients.
+    return ingredients.filter(i => ranges.some(r => r.contains(i))).length;
 }
 
-console.log("Part 1:", part1(input));
-console.log("Part 2:", part2(input));
+function part2(ranges) {
+    // Organize the ranges in ascending order by their starting value.
+    ranges.sort((a, b) => {
+        if (a.start < b.start) {
+            return -1;
+        } if (a.start > b.start) {
+            return 1;
+        }
+        return 0;
+    });
+    // Continuously try to merge ranges, storing them off when we fail.
+    const savedRanges = [];
+    let lastRange = ranges[0];
+    for (let i = 1; i < ranges.length; i++) {
+        const nextRange = ranges[i];
+        const mergeResult = Range.merge(lastRange, nextRange);
+        if (mergeResult.length === 2) {
+            // Merge failed.
+            savedRanges.push(lastRange);
+            lastRange = nextRange;
+            continue;
+        }
+        // Merge succeeded - keep trying to merge into lastRange.
+        lastRange = mergeResult[0];
+    }
+    savedRanges.push(lastRange);
+    // Range from 10-->20 holds 11 values, hence ++.
+    return savedRanges.map(r => ++r.end - r.start).reduce((a, b) => a + b);
+}
+
+const [ranges, ingredients] = parseInput(input);
+console.log("Part 1:", part1(ranges, ingredients));
+console.log("Part 2:", part2(ranges));
--- common/structures/range.js
+++ common/structures/range.js
@@ -1,18 +1,86 @@
 class Range {
-    constructor(start, end) {
-        // Enforce all ranges to be [min, max].
-        const min = Math.min(start, end);
-        const max = Math.max(start, end);
-        this.start = min;
-        this.end = max;
+    constructor(start, end, isBigInt) {
+        this.isBigInt = isBigInt;
+        this.start = isBigInt ? BigInt(start) : Number(start);
+        this.end = isBigInt ? BigInt(end) : Number(end);
+        // Enforce that start is before end.
+        if (this.start > this.end) {
+            throw new Error(`Range start can't be larger than range end: [${start}, ${end}]`)
+        }
     }
 
     consumes(range) {
+        if (this.isBigInt !== range.isBigInt) {
+            throw new Error(`[consume] - Failed on ranges that aren't matching integer types: ${this}, ${range}`);
+        }
         return this.start <= range.start && this.end >= range.end;
     }
 
     overlaps(range) {
+        if (this.isBigInt !== range.isBigInt) {
+            throw new Error(`[overlaps] - Failed on ranges that aren't matching integer types: ${this}, ${range}`);
+        }
         return !(this.start > range.end || this.end < range.start);
+    }
+
+    /**
+     * Merge two ranges together.
+     * 
+     * The result of a merge can take multiple forms - consider ranges [a, b] and [c, d].
+     * 
+     * Case 1 (complete consume): A range falls entirely within another.
+     * [a=3,    b=10]
+     *   [c=5, d=7]
+     * 
+     * Merge here returns [a, b]. This would still return [a, b] if c=3, and b=10.
+     * This always returns 1 range.
+     * 
+     * Case 2 (no overlap): The ranges don't touch at all.
+     * [a=3, b=10]
+     *            [c=11, d=15]
+     * or
+     *            [a=11, b=15]
+     * [c=3, b=10]
+     * 
+     * Merge "fails" here - just return the ranges back to the caller.
+     * This implies that a return length of 2 represents a failed merge.
+     * Callers can use that signal accordingly.
+     * 
+     * Case 3 (partial overlap): The two ranges partially overlap.
+     * [a=3,     b=10]
+     *      [c=7,     d=11]
+     * or
+     *      [a=10,    b=12]
+     * [c=7,    d=11]
+     * 
+     * Merge will coalesce these two ranges together, resulting in a single range
+     * ([a=3, d=11] or [c=7, b=12]).
+     */
+    static merge(r1, r2) {
+        if (r1.isBigInt !== r2.isBigInt) {
+            throw new Error(`[static merge] - Failed on ranges that aren't matching integer types: ${r1}, ${r2}`);
+        }
+        // Case 1
+        if (r1.consumes(r2)) {
+            return [r1];
+        }
+        if (r2.consumes(r1)) {
+            return [r2];
+        }
+        // Case 2 - if r1 overlaps r2, we can assume that r2 overlaps r1. The inverse is true.
+        if (!r1.overlaps(r2)) {
+            return [r1, r2];
+        }
+        // Case 3 - We know that the ranges don't consume, but do overlap.
+        // This means that the overlap between r2 and r1 is enough to determine the final range.
+        if (r1.contains(r2.start)) {
+            return [new Range(r1.start, r2.end, r1.isBigInt)];
+        }
+        return [new Range(r2.start, r1.end, r1.isBigInt)];
+    }
+
+    contains(v) {
+        return this.start <= v && this.end >= v;
     }
 }
 
